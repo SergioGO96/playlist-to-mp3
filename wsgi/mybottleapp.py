6ebd7e7bc348f261a521c70ccccf45e5c1694f2b
Alberto <alberto@MacBook-Air-de-Alberto.local>
2016-06-13 12:00:36 +0200

    prueba listas spotify

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -55,7 +55,7 @@ def personal():
 	if perfil.status_code == 200:
 		cuenta = perfil.json()
 		cuenta = cuenta["id"]
-	listas = requests.get("https://api.spotify.com/v1/users"+str(cuenta)+"playlists", headers=headers)
+	listas = requests.get("https://api.spotify.com/v1/users"+str(cuenta)+"/playlists", headers=headers)
 	if listas.status_code == 200:
 		listas = listas.json()
 	return listas
