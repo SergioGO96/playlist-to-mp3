89aa8a22fa5d46081622d76c8ecc3c6235516b29
Alberto <alberto@MacBook-Air-de-Alberto.local>
2016-06-15 00:37:54 +0200

    Prueba enlace youtube

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -76,7 +76,7 @@ def listas():
 			url_cancion_youtube = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&q='+nombre_cancion.replace(" ","-")+'&maxResults=1&type=video&key=AIzaSyAz9FAUJAgSEDkdE-6wsFhgc18S058wNWU')
 			if url_cancion_youtube.status_code == 200:
 				url_cancion_youtube = url_cancion_youtube.json()
-				lista_canciones.append({"nombre":nombre_cancion,"url_youtube":url_cancion_youtube["items"]})
+				lista_canciones.append({"nombre":nombre_cancion,"url_youtube":url_cancion_youtube["items"][0]["id"]["videoId"]})
 	return template('canciones.tpl',lista_canciones=lista_canciones)
 
 @route('/')
