2571035f74c767fc8de488e7f6d3b4fa3550b800
Alberto <alberto@MacBook-Air-de-Alberto.local>
2016-06-15 01:11:14 +0200

    Añadido campo busqueda youtube

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -79,9 +79,18 @@ def listas():
 				lista_canciones.append({"nombre":nombre_cancion,"url_youtube":url_cancion_youtube["items"][0]["id"]["videoId"]})
 	return template('canciones.tpl',lista_canciones=lista_canciones)
 
-route('/youtube')
+@route('/youtube')
 def youtube():
 	return template('youtube.tpl')
+
+@route('/youtube',method='POST')
+def youtube():
+	video_busqueda = request.forms.get('nombre')
+	url_cancion_youtube = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&q='+video_busqueda.replace(" ","-")+'&maxResults=10&type=video&key=AIzaSyAz9FAUJAgSEDkdE-6wsFhgc18S058wNWU')
+		if url_cancion_youtube.status_code == 200:
+			url_cancion_youtube = url_cancion_youtube.json()
+			lista_videos.append({"nombre":video_busqueda,"url_youtube":url_cancion_youtube["items"][0]["id"]["videoId"]})
+	return template('youtube_videos.tpl',lista_videos=lista_videos)
 
 @route('/')
 def index():
--- wsgi/views/youtube.tpl
+++ wsgi/views/youtube.tpl
@@ -1,5 +1,9 @@
 % include('header.tpl')
     <div class="docs-content">
 		<h2> Buscador de videos en Youtube </h2>
+		<form id="formulario" method="post" action="">
+		  <input type="text" name="nombre" value="" />
+		  <input type="submit" value="Buscar">
+		</form>
 	</div>
 % include('footer.tpl')
